b692b40db97c1b47e777fd3f2a261ce16e20f688
Brad <bkmorse@gmail.com>
2011-11-03 19:57:46 -0400

    initial commit

---
+++ pi.file_directory.php
@@ -0,0 +1,98 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine File Directory Plugin
+ *
+ * @package		File Directory Plugin
+ * @subpackage		Plugins
+ * @category		Plugins
+ * @author		Brad Morse
+ * @link			http://www.bkmorse.com
+ */
+
+$plugin_info = array(
+	'pi_name'			=> 'File Directory',
+	'pi_version'		=> '1.0.0',
+	'pi_author'		=> 'Brad Morse',
+	'pi_author_url'	=> 'http://bkmorse.com',
+	'pi_description'	=> 'Displays all files from within a folder/directory on your server',
+	'pi_usage'		=> File_directory::usage()
+);
+
+
+class File_directory {
+
+	var $return_data;
+
+	/**
+	*  Constructor
+	*/
+	function File_directory()
+	{
+		// make a local reference to the ExpressionEngine super object
+		$this->EE =& get_instance();
+		$directory = ($this->EE->TMPL->fetch_param('directory') !== false) ? $this->EE->TMPL->fetch_param('directory') : "";
+		$wrap = ($this->EE->TMPL->fetch_param('wrap') !== false) ? $this->EE->TMPL->fetch_param('wrap') : "";
+    
+    $return_data = '';
+
+    if($directory) {
+  		//get all files within directory
+      $files = glob($directory);
+    
+      //print each file name
+      foreach($files as $file):
+       if($wrap != '') {
+       $return_data .= '<'.$wrap.'><a href="'.$file.'"></'.$wrap.'>'.$file.'</a></'.$wrap.'>';
+       } else {
+         $return_data .= '<a href="'.$file.'">'.$file.'</a>';
+       }
+      endforeach;
+
+  		$this->return_data = $return_data;
+    }
+	}
+	/* END */
+	
+	
+// ----------------------------------------
+//  Plugin Usage
+// ----------------------------------------
+
+// This function describes how the plugin is used.
+//  Make sure and use output buffering
+
+function usage()
+{
+ob_start(); 
+?>
+Use as follows:
+
+{exp:file_directory directory="/path/to/folder" wrap=""}
+
+directory (required) - absolute or relative path to folder of files you want to display
+
+wrap (optional) - put within paragraph, div or span tags, see below
+
+wrap="p" would put it within <p></p>
+wrap="div" would put it within <div></div>
+wrap="span" would put it within <span></span>
+
+* if you get an error or nothing displays, be sure to check the path you passed to the directory parameter
+
+<?php
+$buffer = ob_get_contents();
+	
+ob_end_clean(); 
+
+return $buffer;
+}
+/* END */
+
+
+}
+// END CLASS
+
+/* End of file pi.pdf_icon.php */
+/* Location: ./system/expressionengine/third_party/file_directory/pi.file_directory.php */
+?>
